a7e0611bf6747dcb18a4eb8e135a9ddf8c12a03a
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2023-04-18 17:14:34 +0200

    Added local storage functionality to the app

--- src/App.js
+++ src/App.js
@@ -3,43 +3,48 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useState, useEffect } from "react";
 
+const getLocalStorage = () => {
+  let tasks = localStorage.getItem("tasks");
+  if (tasks) {
+    return (tasks = JSON.parse(localStorage.getItem("tasks")));
+  } else {
+    return [];
+  }
+};
+
 function App() {
-  const getTasksFromLocalStorage = () => {
-    const tasks = localStorage.getItem("tasks");
-    return tasks ? JSON.parse(tasks) : [];
-  };
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState(getTasksFromLocalStorage());
+  const [tasks, setTasks] = useState(getLocalStorage());
+
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (storedTasks) setTasks(storedTasks);
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  const addTask = (task) => {
-    const newTask = { id: Math.floor(Math.random() * 10000) + 1, ...task };
-    setTasks([...tasks, newTask]);
-    localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
-    getTasksFromLocalStorage()
-  };
-
+  //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  //Toggle reminder
   const toggleReminder = (id) => {
     setTasks(
       tasks.map((task) =>
+        ///...task gets me all task properties and then reminder is changed
         task.id === id ? { ...task, reminder: !task.reminder } : task
       )
     );
-    localStorage.setItem(
-      "tasks",
-      JSON.stringify(
-        tasks.map((task) =>
-          task.id === id ? { ...task, reminder: !task.reminder } : task
-        )
-      )
-    );
+  };
+
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
   };
 
   return (
